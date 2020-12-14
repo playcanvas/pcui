a02e3ca6205ba6e062e1340ce33fb7cf0de2fbc8
Elliott Thompson <ellth92@gmail.com>
2020-12-14 13:18:34 +0000

    add a readme for updating pcui docs

---
+++ docs/README.md
@@ -0,0 +1,48 @@
+
+# PCUI docs guide
+
+### Updating component pages (Optional if you’ve added a new component)
+
+Open `./docs/create-component-pages.js`
+
+Add your new component to the components array (including the category and component name).
+
+`cd ./docs`
+
+`node create-component-pages.js`
+
+Commit any changes to the pcui repo.
+
+### Testing docs locally
+
+If you haven't cloned the (playcanvas.github.io)[https://github.com/playcanvas/playcanvas.github.io] repo, install it locally to a projects folder which will now be referenced as `<projects_folder>`.
+
+In the puci directory run:
+
+`npm run docs:local`
+
+`rm -rf ~/<projects_folder>/playcanvas.github.io/pcui/`
+
+`cp -r ./docs/_site/ ~/<projects_folder>/playcanvas.github.io/pcui/`
+
+`cd ~/<projects_folder>/playcanvas.github.io`
+
+`python -m SimpleHTTPServer 4000`
+
+Visit [localhost:4000/pcui]
+
+### Publishing docs
+
+In the puci main directory run:
+
+`npm run docs:build`
+
+`rm -rf ~/<projects_folder>/playcanvas.github.io/pcui/`
+
+`cp -r ./docs/_site/ ~/<projects_folder>/playcanvas.github.io/pcui/`
+
+`cd ~/<projects_folder>/playcanvas.github.io`
+
+Commit changes to the (playcanvas.github.io)[https://github.com/playcanvas/playcanvas.github.io] repo & create a PR
+
+Upon the PR merging into the master branch of the repo, http://playcanvas.github.io/pcui will be updated.
---
+++ docs/create-component-pages.js
@@ -0,0 +1,58 @@
+var components = [
+    ['input', 'boolean-input'],
+    ['input', 'button'],
+    ['input', 'numeric-input'],
+    ['input', 'select-input'],
+    ['input', 'slider-input'],
+    ['input', 'text-area-input'],
+    ['input', 'text-input'],
+    ['input', 'vector-input'],
+    ['text', 'code'],
+    ['text', 'info-box'],
+    ['text', 'label'],
+    ['layout', 'container'],
+    ['layout', 'divider'],
+    ['layout', 'overlay'],
+    ['layout', 'panel'],
+    ['layout', 'tree-view'],
+    ['misc', 'progress'],
+    ['misc', 'spinner'],
+    ['input', 'context-menu']
+];
+
+var createUpperName = (name) => {
+    var nameparts = name.split('-');
+    var result = '';
+    nameparts.forEach(part => {
+        result = result + part.substring(0, 1).toUpperCase() + part.substring(1, part.length);
+    });
+    return result;
+};
+
+components.forEach(item => {
+    var group = item[0];
+    var name = item[1];
+    var upperName = createUpperName(name);
+    var lowerName = upperName.toLowerCase();
+    var string = `---
+layout: page
+title: ${upperName} 
+permalink: /components/${name}/
+parent: Components
+---
+<iframe src="/pcui/storybook/iframe.html?id=${group}-${lowerName}--main&viewMode=docs" style="height: 100% !important;" class="component-iframe" onload="resize()"></iframe>
+
+<script>
+function resize() {
+    var iframe = document.querySelector('.component-iframe');
+    iframe.setAttribute('style', 'height: ' + iframe.contentDocument.body.offsetHeight + 'px !important; opacity: 1;');
+    iframe.contentDocument.querySelector('.sbdocs-wrapper').setAttribute('style', 'padding-top: 20px;');
+}
+</script>`;
+
+    fs = require('fs');
+    fs.writeFile(`pages/4-components/${upperName}.markdown`, string, function (err) {
+    if (err) return console.log(err);
+    console.log(`written ${upperName}.markdown`);
+    });
+});
