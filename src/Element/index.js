0b3f1d53cc1517f7016bf5d2dd62b07dc77a944d
Elliott Thompson <ellth92@gmail.com>
2022-08-18 15:20:14 +0100

    Element class property (#177)
    * add an element class property

--- src/Element/index.js
+++ src/Element/index.js
@@ -172,14 +172,17 @@ class Element extends Events {
         // add font regular class
         this._dom.classList.add(pcuiClass.FONT_REGULAR);
 
+        this._class = [];
         // add user classes
         if (args.class) {
             if (Array.isArray(args.class)) {
                 for (let i = 0; i < args.class.length; i++) {
                     this._dom.classList.add(args.class[i]);
+                    this._class.push(args.class[i]);
                 }
             } else {
                 this._dom.classList.add(args.class);
+                this._class.push(args.class);
             }
         }
 
@@ -624,6 +627,21 @@ class Element extends Events {
 
     get style() {
         return this._dom.style;
+    }
+
+    set class(value) {
+        if (!Array.isArray(value)) {
+            value = [value];
+        }
+        value.forEach((cls) => {
+            this.classAdd(cls);
+        });
+        this._class.forEach((cls) => {
+            if (!value.includes(cls)) {
+                this.classRemove(cls);
+            }
+        });
+        this._class = value;
     }
 
     get class() {
