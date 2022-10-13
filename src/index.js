ccd8bd4d71a8e01931f49ef81e783030f4578954
Elliott Thompson <ellth92@gmail.com>
2022-10-13 17:40:24 +0100

    package.json update

--- src/index.js
+++ src/index.js
@@ -1,56 +1,52 @@
-import IBindable from './interfaces/IBindable';
-import IFocusable from './interfaces/IFocusable';
-import ICollapsible from './interfaces/ICollapsible';
-import IFlex from './interfaces/IFlex';
-import IGrid from './interfaces/IGrid';
-import IScrollable from './interfaces/IScrollable';
-import IResizable from './interfaces/IResizable';
-
-import Element from './Element';
-import TextInput from './TextInput';
-import TextAreaInput from './TextAreaInput';
-import NumericInput from './NumericInput';
-import SliderInput from './SliderInput';
-import VectorInput from './VectorInput';
-import BooleanInput from './BooleanInput';
-import Label from './Label';
-import Button from './Button';
-import RadioButton from './RadioButton';
-import Canvas from './Canvas';
-import Code from './Code';
-import ColorPicker from './ColorPicker';
-import Container from './Container';
-import Panel from './Panel';
-import Overlay from './Overlay';
-import Divider from './Divider';
-import InfoBox from './InfoBox';
-import SelectInput from './SelectInput';
-import Spinner from './Spinner';
-import Progress from './Progress';
-import ContextMenu from './ContextMenu';
-import TreeView from './TreeView';
-import TreeViewItem from './TreeViewItem';
-import LabelGroup from './LabelGroup';
-import GradientPicker from './GradientPicker';
-import GridView from './GridView';
-import GridViewItem from './GridViewItem';
 import ArrayInput from './ArrayInput';
-import MenuItem from './MenuItem';
-import Menu from './Menu';
 import BindingBase from './BindingBase';
 import BindingElementToObservers from './BindingElementToObservers';
 import BindingObserversToElement from './BindingObserversToElement';
 import BindingTwoWay from './BindingTwoWay';
+import BooleanInput from './BooleanInput';
+import Button from './Button';
+import Canvas from './Canvas';
+import Code from './Code';
+import ColorPicker from './ColorPicker';
+import Container from './Container';
+import ContextMenu from './ContextMenu';
+import Divider from './Divider';
+import Element from './Element';
+import GradientPicker from './GradientPicker';
+import GridView from './GridView';
+import GridViewItem from './GridViewItem';
+import InfoBox from './InfoBox';
+import IBindable from './interfaces/IBindable';
+import ICollapsible from './interfaces/ICollapsible';
+import IFlex from './interfaces/IFlex';
+import IFocusable from './interfaces/IFocusable';
+import IGrid from './interfaces/IGrid';
+import IResizable from './interfaces/IResizable';
+import IScrollable from './interfaces/IScrollable';
+import Label from './Label';
+import LabelGroup from './LabelGroup';
+import Menu from './Menu';
+import MenuItem from './MenuItem';
+import NumericInput from './NumericInput';
+import Overlay from './Overlay';
+import Panel from './Panel';
+import Progress from './Progress';
+import RadioButton from './RadioButton';
+import SelectInput from './SelectInput';
+import SliderInput from './SliderInput';
+import Spinner from './Spinner';
+import TextAreaInput from './TextAreaInput';
+import TextInput from './TextInput';
+import TreeView from './TreeView';
+import TreeViewItem from './TreeViewItem';
+import VectorInput from './VectorInput';
 
 export {
-    IBindable,
-    IFocusable,
-    ICollapsible,
-    IScrollable,
-    IResizable,
-    IFlex,
-    IGrid,
     ArrayInput,
+    BindingBase,
+    BindingElementToObservers,
+    BindingObserversToElement,
+    BindingTwoWay,
     BooleanInput,
     Button,
     Canvas,
@@ -61,7 +57,16 @@ export {
     Divider,
     Element,
     GradientPicker,
+    GridView,
+    GridViewItem,
+    IBindable,
+    ICollapsible,
+    IFlex,
+    IFocusable,
+    IGrid,
     InfoBox,
+    IResizable,
+    IScrollable,
     Label,
     LabelGroup,
     Menu,
@@ -78,11 +83,5 @@ export {
     TextInput,
     TreeView,
     TreeViewItem,
-    GridView,
-    GridViewItem,
-    VectorInput,
-    BindingBase,
-    BindingElementToObservers,
-    BindingObserversToElement,
-    BindingTwoWay
+    VectorInput
 };
