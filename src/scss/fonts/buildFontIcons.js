b2158eeea1b807047bf605d758e779af7e7446c4
Will Eastcott <will@playcanvas.com>
2022-12-20 11:09:20 +0000

    Add quotes around url param

--- src/scss/fonts/buildFontIcons.js
+++ src/scss/fonts/buildFontIcons.js
@@ -2,5 +2,5 @@ const fs = require('fs');
 
 const file = fs.readFileSync('./PlayIcons-Regular.woff');
 const b64FontString = file.toString('base64');
-const style = `$playIconsRegularWoff: url(data:application/font-woff;charset=utf-7;base64,${b64FontString}) format('woff');\n`;
+const style = `$playIconsRegularWoff: url('data:application/font-woff;charset=utf-7;base64,${b64FontString}') format('woff');\n`;
 fs.writeFileSync('./PlayIcons-Regular-woff.scss', style);
