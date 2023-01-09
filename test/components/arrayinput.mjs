5bf929d7d51fa7257e7a0ef62beebda633975d70
Will Eastcott <will@playcanvas.com>
2023-01-09 14:47:51 +0000

    Fix ArrayInput test

--- test/components/arrayinput.mjs
+++ test/components/arrayinput.mjs
@@ -9,5 +9,6 @@ test('ArrayInput defaults', () => {
     const arrayInput = new ArrayInput();
 
     assert.strictEqual(arrayInput.renderChanges, false);
-    assert.equal(arrayInput.value, []);
+    assert.strictEqual(arrayInput.value instanceof Array, true);
+    assert.strictEqual(arrayInput.value.length, 0);
 });
