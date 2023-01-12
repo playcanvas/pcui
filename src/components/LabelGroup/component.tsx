574514bf9c400bd91d1d7045474721187e8acb17
Elliott Thompson <ellth92@gmail.com>
2023-01-12 16:06:41 +0000

    fix the linking of react label group fields

--- src/components/LabelGroup/component.tsx
+++ src/components/LabelGroup/component.tsx
@@ -16,13 +16,17 @@ class LabelGroup extends BaseComponent <LabelGroupArgs, any> {
         if (Array.isArray(this.props.children) || !this.props.children) {
             throw new Error('A LabelGroup must contain a single child react component');
         }
+        const labelFieldChild = (this.props.children as { type: any, props: any });
+        const labelField = new labelFieldChild.type.ctor(labelFieldChild.props);
+        if (labelFieldChild.props.link) {
+            labelField.link(labelFieldChild.props.link.observer, labelFieldChild.props.link.path);
+        }
         this.element = new this.elementClass({
             ...this.props,
             dom: nodeElement,
             container: containerElement,
             parent: undefined,
-            // @ts-ignore
-            field: new this.props.children.type.ctor(this.props.children.props)
+            field: labelField
         });
     };
 
