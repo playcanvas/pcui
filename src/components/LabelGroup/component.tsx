db479a5ea5e76ab1ebda1bd1021e2275364825ff
Elliott Thompson <ellth92@gmail.com>
2023-01-12 18:18:53 +0000

    fixes

--- src/components/LabelGroup/component.tsx
+++ src/components/LabelGroup/component.tsx
@@ -1,6 +1,6 @@
 import Element, { LabelGroupArgs } from './index';
 import BaseComponent from '../Element/component';
-import { ReactElement } from 'react';
+import { JSXElementConstructor, ReactElement } from 'react';
 
 /**
  * Represents a group of a Label and a Element. Useful for rows of labeled fields.
@@ -23,14 +23,14 @@ class LabelGroup extends BaseComponent <LabelGroupArgs, any> {
         const child = this.props.children as ReactElement;
         const fieldProps = child.props as Record<any, any>;
         // check if the ReactElement contains an instance of a BaseComponent as its type, confirming it is a PCUI react component
-        if (!(child.type instanceof BaseComponent)) {
+        if (!((child.type as JSXElementConstructor<any>).prototype instanceof BaseComponent)) {
             throw new Error(childrenErrorMessage);
         }
         // it's safe to cast the ReactElement child as a BaseComponent as we have confirmed it is a BaseComponent above
         const fieldClass = (child.type as unknown as typeof BaseComponent).ctor;
         const labelField = new fieldClass({ ...fieldProps });
         if (child.props.link) {
-            labelField.link(fieldProps.link.observer, fieldProps.props.link.path);
+            labelField.link(fieldProps.link.observer, fieldProps.link.path);
         }
         this.element = new this.elementClass({
             ...this.props,
