559f3ba8e885e2dd9075f2f3adc5c63ef1273ad1
Elliott Thompson <ellth92@gmail.com>
2023-01-12 18:05:37 +0000

    fix for label group field properties and the type of the label group field

--- src/components/LabelGroup/component.tsx
+++ src/components/LabelGroup/component.tsx
@@ -1,5 +1,6 @@
 import Element, { LabelGroupArgs } from './index';
 import BaseComponent from '../Element/component';
+import { ReactElement } from 'react';
 
 /**
  * Represents a group of a Label and a Element. Useful for rows of labeled fields.
@@ -13,13 +14,23 @@ class LabelGroup extends BaseComponent <LabelGroupArgs, any> {
 
     attachElement = (nodeElement: HTMLElement, containerElement: any) => {
         if (!nodeElement) return;
+        const childrenErrorMessage = 'A LabelGroup must contain a single PCUI react component as a child';
+        // check that the LabelGroup has a single child
         if (Array.isArray(this.props.children) || !this.props.children) {
-            throw new Error('A LabelGroup must contain a single child react component');
+            throw new Error(childrenErrorMessage);
         }
-        const labelFieldChild = (this.props.children as { type: any, props: any });
-        const labelField = new labelFieldChild.type.ctor(labelFieldChild.props);
-        if (labelFieldChild.props.link) {
-            labelField.link(labelFieldChild.props.link.observer, labelFieldChild.props.link.path);
+        // casting child as a single ReactElement as we have confirmed it is above
+        const child = this.props.children as ReactElement;
+        const fieldProps = child.props as Record<any, any>;
+        // check if the ReactElement contains an instance of a BaseComponent as its type, confirming it is a PCUI react component
+        if (!(child.type instanceof BaseComponent)) {
+            throw new Error(childrenErrorMessage);
+        }
+        // it's safe to cast the ReactElement child as a BaseComponent as we have confirmed it is a BaseComponent above
+        const fieldClass = (child.type as unknown as typeof BaseComponent).ctor;
+        const labelField = new fieldClass({ ...fieldProps });
+        if (child.props.link) {
+            labelField.link(fieldProps.link.observer, fieldProps.props.link.path);
         }
         this.element = new this.elementClass({
             ...this.props,
