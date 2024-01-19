3f6c9a9cbeb00ec505a9f620e1a8866aaa92deec
Will Eastcott <will@playcanvas.com>
2024-01-19 09:37:32 +0000

    Lint fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * PCUI is a front-end framework designed for creating user interfaces in web applications. It is
  * particularly well-suited for building browser-based tools. It offers a comprehensive set of UI
  * components like buttons, sliders, menus and data inputs.
- * 
+ *
  * PCUI is written in TypeScript. The API can be used from both TypeScript and JavaScript. A React
  * wrapper is provided for easy integration with React applications.
  *
