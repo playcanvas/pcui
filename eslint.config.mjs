d1a4499573cd869181730ce6a4bc346b71d2f8bb
Will Eastcott <will@playcanvas.com>
2024-11-08 15:16:13 +0000

    Add ESLint config

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import playcanvasConfig from '@playcanvas/eslint-config';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+
+export default [
+    ...playcanvasConfig,
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+                project: true
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.mocha,
+                ...globals.node
+            }
+        },
+        plugins: {
+            '@typescript-eslint': tsPlugin
+        },
+        rules: {
+            ...tsPlugin.configs['recommended'].rules,
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'import/extensions': 'off',
+            'import/no-unresolved': 'off',
+            'jsdoc/require-param-type': 'off',
+            'jsdoc/require-returns': 'off',
+            'jsdoc/require-returns-type': 'off'
+        }
+    }
+];
