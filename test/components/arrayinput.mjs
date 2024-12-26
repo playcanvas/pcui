283372d249f1ecb966cefaf1f2df1023ea4dc981
Will Eastcott <will@playcanvas.com>
2024-12-26 19:44:26 +0000

    Add unit tests for NumericInput expression handling

--- test/components/arrayinput.mjs
+++ test/components/arrayinput.mjs
@@ -1,22 +1,24 @@
-import test from 'node:test';
+import { describe, it } from 'node:test';
 import { strictEqual } from 'assert';
 
 import 'global-jsdom/register'
 
 import { ArrayInput } from '../../dist/module/src/components/ArrayInput/index.mjs';
 
-test('ArrayInput constructor: no args', () => {
-    const arrayInput = new ArrayInput();
+describe('ArrayInput', () => {
+    it('constructor: no args', () => {
+        const arrayInput = new ArrayInput();
 
-    strictEqual(arrayInput.class.length, 6);
-    strictEqual(arrayInput.class.contains('font-regular'), true);
-    strictEqual(arrayInput.class.contains('pcui-array-empty'), true);
-    strictEqual(arrayInput.class.contains('pcui-array-input'), true);
-    strictEqual(arrayInput.class.contains('pcui-container'), true);
-    strictEqual(arrayInput.class.contains('pcui-element'), true);
-    strictEqual(arrayInput.class.contains('pcui-flex'), true);
+        strictEqual(arrayInput.class.length, 6);
+        strictEqual(arrayInput.class.contains('font-regular'), true);
+        strictEqual(arrayInput.class.contains('pcui-array-empty'), true);
+        strictEqual(arrayInput.class.contains('pcui-array-input'), true);
+        strictEqual(arrayInput.class.contains('pcui-container'), true);
+        strictEqual(arrayInput.class.contains('pcui-element'), true);
+        strictEqual(arrayInput.class.contains('pcui-flex'), true);
 
-    strictEqual(arrayInput.renderChanges, false);
-    strictEqual(arrayInput.value instanceof Array, true);
-    strictEqual(arrayInput.value.length, 0);
+        strictEqual(arrayInput.renderChanges, false);
+        strictEqual(arrayInput.value instanceof Array, true);
+        strictEqual(arrayInput.value.length, 0);
+    });
 });
