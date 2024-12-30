7fb74e9dc58c04ada9dc444f9e895364de24725b
Will Eastcott <will@playcanvas.com>
2024-12-30 22:43:30 +0000

    Reorganize SCSS files

--- utils/build-font-icons.mjs
+++ utils/build-font-icons.mjs
@@ -3,4 +3,4 @@ import fs from 'fs';
 const file = fs.readFileSync('./PlayIcons-Regular.woff');
 const b64FontString = file.toString('base64');
 const style = `$playicons-regular: url('data:application/font-woff;charset=utf-7;base64,${b64FontString}') format('woff');\n`;
-fs.writeFileSync('../src/scss/fonts/_playicons-regular.scss', style);
+fs.writeFileSync('../src/scss/_icons.scss', style);
