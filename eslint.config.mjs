92c9e40ef8936661382895ab1a53c0516d483454
Will Eastcott <will@playcanvas.com>
2025-11-08 12:10:37 +0000

    Reformat eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,46 +6,50 @@ import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import globals from 'globals';
 
-export default [...playcanvasConfig, {
-    files: ['src/**/*.ts'],
-    languageOptions: {
-        parser: tsParser,
-        parserOptions: {
-            ecmaVersion: 2022,
-            sourceType: 'module',
-            project: true
+export default [
+    ...playcanvasConfig,
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+                project: true
+            },
+            globals: {
+                ...globals.browser,
+                ...globals.mocha,
+                ...globals.node
+            }
         },
-        globals: {
-            ...globals.browser,
-            ...globals.mocha,
-            ...globals.node
+        plugins: {
+            '@typescript-eslint': tsPlugin
+        },
+        settings: {
+            'import/resolver': {
+                typescript: {}
+            }
+        },
+        rules: {
+            ...tsPlugin.configs.recommended.rules,
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'jsdoc/require-param-type': 'off',
+            'jsdoc/require-returns': 'off',
+            'jsdoc/require-returns-type': 'off'
+        }
+    }, {
+        files: ['**/*.mjs'],
+        languageOptions: {
+            globals: {
+                ...globals.node
+            }
+        },
+        rules: {
+            'import/no-unresolved': 'off'
         }
     },
-    plugins: {
-        '@typescript-eslint': tsPlugin
-    },
-    settings: {
-        'import/resolver': {
-            typescript: {}
-        }
-    },
-    rules: {
-        ...tsPlugin.configs.recommended.rules,
-        '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        'jsdoc/require-param-type': 'off',
-        'jsdoc/require-returns': 'off',
-        'jsdoc/require-returns-type': 'off'
-    }
-}, {
-    files: ['**/*.mjs'],
-    languageOptions: {
-        globals: {
-            ...globals.node
-        }
-    },
-    rules: {
-        'import/no-unresolved': 'off'
-    }
-}, ...storybook.configs["flat/recommended"]];
+    ...storybook.configs["flat/recommended"]
+];
